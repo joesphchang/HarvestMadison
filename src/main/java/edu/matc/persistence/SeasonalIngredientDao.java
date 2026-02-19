f6ebbe6c76d228dbd48181d9e85b49cbe26fda26
joesphchang <joesphschang@gmail.com>
2026-02-19 00:02:21 -0600

    added boilerplate code to seasonalingredientdao

--- src/main/java/edu/matc/persistence/SeasonalIngredientDao.java
+++ src/main/java/edu/matc/persistence/SeasonalIngredientDao.java
@@ -1,4 +1,14 @@
 package edu.matc.persistence;
 
-public class SeasonalIngredientDao {
+import edu.matc.entity.SeasonalIngredient;
+
+public class SeasonalIngredientDao extends GenericDao<SeasonalIngredient> {
+
+    public SeasonalIngredientDao() {
+        super(SeasonalIngredient.class);
+    }
+
+    public SeasonalIngredient getIngredientByMonth(int month) {
+
+    }
 }
