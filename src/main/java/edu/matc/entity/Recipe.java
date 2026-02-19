12b10112d2c9dd765708ab584bb94103413f8fe0
joesphchang <joesphschang@gmail.com>
2026-02-18 22:42:14 -0600

    finished my Recipe entity

--- src/main/java/edu/matc/entity/Recipe.java
+++ src/main/java/edu/matc/entity/Recipe.java
@@ -1,5 +1,126 @@
 package edu.matc.entity;
 
-public class Recipe {
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
 
+/**
+ * The type Recipe.
+ */
+@Entity(name = "Recipe")
+@Table(name = "recipe")
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "recipe_name")
+    private String recipeName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_on")
+    private int createdOn;
+
+    /**
+     * Instantiates a new Recipe.
+     */
+    public Recipe() {
+    }
+
+    /**
+     * Instantiates a new Recipe.
+     *
+     * @param id          the id
+     * @param recipeName  the recipe name
+     * @param description the description
+     */
+    public Recipe(int id, String recipeName, String description) {
+        this.id = id;
+        this.recipeName = recipeName;
+        this.description = description;
+    }
+
+    /**
+     * Gets created on.
+     *
+     * @return the created on
+     */
+    public int getCreatedOn() {
+        return createdOn;
+    }
+
+    /**
+     * Sets created on.
+     *
+     * @param createdOn the created on
+     */
+    public void setCreatedOn(int createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets description.
+     *
+     * @param description the description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Gets recipe name.
+     *
+     * @return the recipe name
+     */
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    /**
+     * Sets recipe name.
+     *
+     * @param recipeName the recipe name
+     */
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", recipeName='" + recipeName + '\'' +
+                ", description='" + description + '\'' +
+                ", createdOn=" + createdOn +
+                '}';
+    }
 }
