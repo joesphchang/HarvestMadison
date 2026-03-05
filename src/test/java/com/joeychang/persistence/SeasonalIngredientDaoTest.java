c0cbe5dc3084213781415ba02d4c5d91730b8bf8
joesphchang <joesphschang@gmail.com>
2026-03-04 21:58:27 -0600

    added in erd diagram within design documents

--- src/test/java/com/joeychang/persistence/SeasonalIngredientDaoTest.java
+++ src/test/java/com/joeychang/persistence/SeasonalIngredientDaoTest.java
@@ -9,10 +9,19 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * The type Seasonal ingredient dao test.
+ */
 public class SeasonalIngredientDaoTest {
 
+    /**
+     * The Seasonal ingredient dao.
+     */
     GenericDao<SeasonalIngredient> seasonalIngredientDao;
 
+    /**
+     * Sets up.
+     */
     @BeforeEach
     void setUp() {
         Database database = Database.getInstance();
@@ -20,12 +29,18 @@ public class SeasonalIngredientDaoTest {
         seasonalIngredientDao = new GenericDao<>(SeasonalIngredient.class);
     }
 
+    /**
+     * Gets all seasonal ingredients.
+     */
     @Test
     void getAllSeasonalIngredients() {
         List<SeasonalIngredient> seasonalIngredients = seasonalIngredientDao.getAll();
         assertEquals(12, seasonalIngredients.size());
     }
 
+    /**
+     * Gets seasonal ingredient by id success.
+     */
     @Test
     void getSeasonalIngredientByIdSuccess() {
         SeasonalIngredient retrievedIngredient = (SeasonalIngredient)seasonalIngredientDao.getById(8);
@@ -33,11 +48,38 @@ public class SeasonalIngredientDaoTest {
         assertEquals("Sweet Corn", retrievedIngredient.getSeasonalIngredientName());
     }
 
+    /**
+     * Gets by property by equal.
+     */
     @Test
     void getByPropertyByEqual() {
       List<SeasonalIngredient> seasonalIngredients = seasonalIngredientDao.findByPropertyEqual("seasonalIngredientName", "Microgreens");
       assertEquals(1, seasonalIngredients.size());
       assertEquals(3, seasonalIngredients.get(0).getId());
+    }
 
+    /**
+     * Gets beets by start month.
+     */
+    @Test
+    void getBeetsByStartMonth() {
+        int december = 12;
+
+        List<SeasonalIngredient> results = seasonalIngredientDao.findByPropertyEqual("startDate", 12);
+        assertEquals(1, results.size());
+        assertEquals("Beets", results.get(0).getSeasonalIngredientName());
+        assertEquals(1, results.get(0).getEndDate());
+    }
+
+    /**
+     * Gets ingredients by current month success.
+     */
+    @Test
+    void getIngredientsByCurrentMonthSuccess() {
+        int testCurrentMonth = 10;
+
+        List<SeasonalIngredient> seasonalIngredients = seasonalIngredientDao.findByPropertyEqual("startDate", testCurrentMonth);
+        assertFalse(seasonalIngredients.isEmpty());
+        assertEquals("Pumpkins", seasonalIngredients.get(0).getSeasonalIngredientName());
     }
 }
