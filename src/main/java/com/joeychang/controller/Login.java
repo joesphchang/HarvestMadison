9dded1ba0efc7e1800e912f8eaee20658dc5ce24
joesphchang <joesphschang@gmail.com>
2026-03-08 10:53:48 -0500

    updated git ignore

--- src/main/java/com/joeychang/controller/Login.java
+++ src/main/java/com/joeychang/controller/Login.java
@@ -1,4 +1,35 @@
 package com.joeychang.controller;
 
-public class Login {
+import com.joeychang.utilities.PropertiesLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Properties;
+
+@WebServlet(
+        urlPatterns = {"/login"}
+)
+/**
+ * Begins the authentication process using AWS Cognito
+ *
+ */
+public class Login extends HttpServlet implements PropertiesLoader {
+    Properties properties;
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    public static String CLIENT_ID;
+    public static String LOGIN_URL;
+    public static String REDIRECT_URL;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        loadProperties();
+    }
+
 }
