28bbab5fc983306c2e03cf696ba2b2e9fceff836
joesphchang <joesphschang@gmail.com>
2026-03-15 22:01:29 -0500

    added in ingredient entity

--- src/main/java/com/joeychang/entity/Ingredient.java
+++ src/main/java/com/joeychang/entity/Ingredient.java
@@ -1,4 +1,48 @@
 package com.joeychang.entity;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity(name = "Ingredient")
+@Table(name = "ingredient")
 public class Ingredient {
+
+    @id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Ingredient() {
+    }
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
